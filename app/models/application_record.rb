6f11ab361c4339b009ece7ddf522aebf873b7e60
Max Morton <max.morton.42@gmail.com>
2019-11-12 17:15:31 -0700

    styled

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  # called on an instance @person.full_name
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+
+ end
 end
